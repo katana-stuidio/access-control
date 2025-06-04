b8c46084819a63e5164ec714a311bdd10009bd14
Charles Tenorio <charlestenorio@MacBook-Pro-de-Charles.local>
2025-06-04 08:08:54 -0300

    config de acesso cros

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -32,6 +32,20 @@ func main() {
 	// Criação do router com Gin
 	router := gin.Default()
 
+	// Configure CORS
+	router.Use(func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE, PATCH")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	})
+
 	// Healthcheck básico
 	router.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
